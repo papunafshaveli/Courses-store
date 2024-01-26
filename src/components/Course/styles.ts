aaec4ea2305d2bfe83eb37542af3297f3c622edb
Papuna Fshaveli <papunafshaveli@gmail.com>
2024-01-26 15:01:40 +0400

    scale edit

--- src/components/Course/styles.ts
+++ src/components/Course/styles.ts
@@ -9,6 +9,12 @@ export const SingleCourse = styled.div<{ $currency: string }>`
   border: none;
   border-radius: 6px;
 
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+
   box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
     rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
     rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
