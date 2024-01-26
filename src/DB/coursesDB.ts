5d127851b4a44680f58f97d7b81e2e96698e16fb
Papuna Fshaveli <papunafshaveli@gmail.com>
2024-01-26 13:15:34 +0400

    edit DB

--- src/DB/coursesDB.ts
+++ src/DB/coursesDB.ts
@@ -9,6 +9,21 @@ import GitGitHub from "../images/GitGitHub.png";
 import kotlin from "../images/kotlin.png";
 import php from "../images/php.png";
 import swift from "../images/swift.jpeg";
+import dart from "../images/dart.png";
+import delph from "../images/delph.png";
+import haskel from "../images/haskel.png";
+import matlab from "../images/matlab.jpeg";
+import nosql from "../images/nosql.png";
+import perl from "../images/perl.jpeg";
+import power from "../images/power.png";
+import rust from "../images/rust.jpeg";
+import scala from "../images/power.png";
+import apl from "../images/apl.svg";
+import cplus from "../images/cplus.png";
+import awk from "../images/awk.png";
+import jquery from "../images/jquery.jpeg";
+import python from "../images/python.png";
+import f from "../images/f.png";
 // Export DB
 export const coursesDB = [
   {
@@ -62,98 +77,98 @@ export const coursesDB = [
   {
     id: 9,
     title: "Swift - Always New!",
-    price: 24.99,
+    price: 29.99,
     img: swift,
   },
   {
     id: 10,
-    title: "Advance CSS and Sass",
-    price: 12.99,
-    img: Sass,
+    title: "The Road to dart",
+    price: 26.99,
+    img: dart,
   },
   {
     id: 11,
-    title: "Bootstrap5",
-    price: 10.99,
-    img: Bootstrap5,
+    title: "The Road to delph",
+    price: 11.99,
+    img: delph,
   },
   {
     id: 12,
-    title: "Git and GitHub",
-    price: 10.99,
-    img: GitGitHub,
+    title: "The Road to haskel",
+    price: 23.99,
+    img: haskel,
   },
 
   {
     id: 13,
-    title: "The Road to React",
-    price: 19.99,
-    img: react,
+    title: "The Road to matlab",
+    price: 18.99,
+    img: matlab,
   },
   {
     id: 14,
-    title: "The Rout to GraphQL",
-    price: 29.99,
-    img: graphql,
+    title: "The Road to nosql",
+    price: 25.99,
+    img: nosql,
   },
   {
     id: 15,
-    title: "The Road to JavaScript",
-    price: 14.99,
-    img: js,
+    title: "The Road to perl",
+    price: 12.99,
+    img: perl,
   },
   {
     id: 16,
-    title: "Advance CSS and Sass",
-    price: 12.99,
-    img: Sass,
+    title: "The Road to power",
+    price: 29.99,
+    img: power,
   },
   {
     id: 17,
-    title: "Bootstrap5",
-    price: 10.99,
-    img: Bootstrap5,
+    title: "The Road to rust",
+    price: 14.99,
+    img: rust,
   },
   {
     id: 18,
-    title: "Git and GitHub",
-    price: 10.99,
-    img: GitGitHub,
+    title: "The Road to scala",
+    price: 33.99,
+    img: scala,
   },
   {
     id: 19,
-    title: "The Road to React",
-    price: 19.99,
-    img: react,
+    title: "The Road to apl",
+    price: 16.99,
+    img: apl,
   },
   {
     id: 20,
-    title: "The Rout to GraphQL",
-    price: 29.99,
-    img: graphql,
+    title: "The Road to C++",
+    price: 28.99,
+    img: cplus,
   },
   {
     id: 21,
-    title: "The Road to JavaScript",
-    price: 14.99,
-    img: js,
+    title: "The Road to awk",
+    price: 17.99,
+    img: awk,
   },
   {
     id: 22,
-    title: "Advance CSS and Sass",
-    price: 12.99,
-    img: Sass,
+    title: "The Road to jquery",
+    price: 34.99,
+    img: jquery,
   },
   {
     id: 23,
-    title: "Bootstrap5",
-    price: 10.99,
-    img: Bootstrap5,
+    title: "The Road to python",
+    price: 17.99,
+    img: python,
   },
   {
     id: 24,
-    title: "Git and GitHub",
-    price: 10.99,
-    img: GitGitHub,
+    title: "The Road to F#",
+    price: 16.99,
+    img: f,
   },
 ];
--- src/pages/CourseDetails/CourseDetails.tsx
+++ src/pages/CourseDetails/CourseDetails.tsx
@@ -1,13 +1,20 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 const CourseDetails = () => {
   const { courseId } = useParams();
   console.log(courseId);
   return (
-    <div>
-      <p>{courseId}</p>
-    </div>
+    <CourseDetailsContainer>
+      <h1>{courseId}</h1>
+    </CourseDetailsContainer>
   );
 };
 
 export default CourseDetails;
+
+const CourseDetailsContainer = styled.div`
+  background-color: #88886f;
+
+  height: 100vh;
+`;
